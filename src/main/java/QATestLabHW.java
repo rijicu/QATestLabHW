050ed1ef3cf686ad5002cf9c5055322e401f4766
rijicu <rijicu@mail.ru>
2018-08-20 18:54:10 +0300

    part1

--- src/main/java/QATestLabHW.java
+++ src/main/java/QATestLabHW.java
@@ -1,4 +1,6 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
@@ -6,12 +8,37 @@ import org.openqa.selenium.firefox.FirefoxDriver;
  * Created by stoykov on 14.08.2018.
  */
 public class QATestLabHW {
-    public static void main(String[] args) {
+
+
+
+    public static void main(String[] args) throws InterruptedException {
+        String userAdminEmail = "webinar.test@gmail.com";
+        String userAdminPass = "Xcg7299bnSmMuRLp9ITw";
+
         WebDriver driver = getChromeDriver();
         driver.manage().window().maximize();
+        driver.get("http://prestashop-automation.qatestlab.com.ua/admin147ajyvk0/");
+        WebElement emailInput = driver.findElement(By.id("email"));
+        WebElement passInput = driver.findElement(By.id("passwd"));
+        WebElement submitButton = driver.findElement(By.name("submitLogin"));
 
 
-        driver.get("https://www.bing.com/");
+        emailInput.clear();
+        emailInput.sendKeys(userAdminEmail);
+        passInput.clear();
+        passInput.sendKeys(userAdminPass);
+        submitButton.click();
+        waitElementToLoad(2000);
+
+        WebElement userBox = driver.findElement(By.id("header_employee_box"));
+        userBox.click();
+        waitElementToLoad(2000);
+
+        WebElement exitButton = driver.findElement(By.id("header_logout"));
+        exitButton.click();
+
+
+        driver.quit();
 
     }
 
@@ -26,4 +53,9 @@ public class QATestLabHW {
         System.setProperty("webdriver.gecko.driver", QATestLabHW.class.getResource("geckodriver.exe").getPath());
         return new FirefoxDriver();
     }
+
+    public static void waitElementToLoad(int milliseconds) throws InterruptedException {
+        Thread.sleep(milliseconds);
+    }
+
 }
