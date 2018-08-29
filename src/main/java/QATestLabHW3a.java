8e38810146364561bd9d0179a31998cc517fb657
rijicu <rijicu@mail.ru>
2018-08-29 23:03:16 +0300

    HW3a

--- src/main/java/QATestLabHW3a.java
+++ src/main/java/QATestLabHW3a.java
@@ -1,4 +1,3 @@
-import com.sun.xml.internal.ws.encoding.MtomCodec;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -6,14 +5,9 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.events.EventFiringWebDriver;
-import org.openqa.selenium.support.events.WebDriverEventListener;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
-import java.beans.EventHandler;
 import java.util.concurrent.TimeUnit;
-import java.util.logging.LogManager;
-import java.util.logging.Logger;
 
 /**
  * Created by stoykov on 29.08.2018.
